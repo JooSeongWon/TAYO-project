b39748600333da61004b9136f5173616758e7017
JooSeongWon <scan0359@naver.com>
2021-11-24 14:14:15 +0900

    admin-member grade,ban,search

--- src/main/webapp/resources/js/admin-members-script.js
+++ src/main/webapp/resources/js/admin-members-script.js
@@ -1,3 +1,66 @@
-$(".change").click(function() {
-	$("form").submit();
+
+$(".grade").change(function() {
+	
+	let grade = $(this).val();
+	let memberId = $(this).attr('id');
+	
+	console.log(grade)
+	console.log(memberId)
+    $.ajax({
+       type: "post"
+       , url: "/admin/members/"+ memberId
+       , data: {
+          grade : grade
+       }
+    ,  dataType:"text" //응답 데이터 형식
+    ,  success: function( res ) {
+    	console.log(res)
+
+    }
+   , error: function() {
+      console.log("에러 발생")
+   }
+       
+    })
+   
+}); //$(".grade").change
+
+$(".ban").change(function() {
+	
+	let ban = $(this).val();
+	let memberId = $(this).attr('id');
+	
+	console.log(ban)
+	console.log(memberId)
+    $.ajax({
+       type: "post"
+       , url: "/admin/members/"+ memberId
+       , data: {
+          ban : ban
+       }
+    ,  dataType:"text" //응답 데이터 형식
+    ,  success: function( res ) {
+    	console.log(res)
+
+    }
+   , error: function() {
+      console.log("에러 발생")
+   }
+       
+    })
+   
+}); //$(".ban").change
+
+$("#btnSearch").click(function() {
+	location.href="/admin/members/?search="+$("#search").val();
 });
+
+$("#search").val("");
+$("#search").focus();
+
+function enterkey() {
+	if (window.event.keyCode == 13) {
+		location.href="/admin/members/?search="+$("#search").val();
+    }
+}
+
